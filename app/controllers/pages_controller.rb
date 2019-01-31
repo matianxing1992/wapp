94d585e238dbf8af74e67e31a77735792e247ec6
Tianxing <matianxing1992@gmail.com>
2019-01-31 08:48:03 +0000

    Step 3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
+
   def home
+
+  end
+
+  def tma1
+    # render 'profile.html.erb'
+  end
+
+  def review
+    # render 'review.html.erb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root to: "page#home"
+
+  get "tma1", to: "pages#tma1", as: "tma1"
+  get "review", to: "pages#review", as: "review"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
